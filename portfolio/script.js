7e60defba53af53b3980cb175e1b4cc8d00c51c4
Madina <madinaeleubai@gmail.com>
2024-03-26 16:22:57 +0600

    add

---
+++ portfolio/script.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function darkMode() {
+  let element = document.body;
+  let content = document.getElementById("DarkMode");
+  element.className = "dark-mode";
+  content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+  let element = document.body;
+  let content = document.getElementById("DarkModetext");
+  element.className = "light-mode";
+  content.innerText = "Dark Mode is OFF";
+}
